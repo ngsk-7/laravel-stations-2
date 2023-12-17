437a473cc8015bb62c29f06ebc3d738a54e61f33
Tomoyuki Nagasaka <t_nagasaka@satt.jp>
2023-12-17 20:32:50 +0900

    laravel question4

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Practice;
 
 class PracticeController extends Controller
 {
@@ -18,5 +19,10 @@ class PracticeController extends Controller
     public function sample3(){
         $test = 'test';
         return view('practice2',['testParam'=>$test]);
+    }
+
+    public function getPractice(){
+        $practice = Practice::all();
+        return response()->json($practice);
     }
 }
